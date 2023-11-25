0619d553412eeab9e2fe9358abfd6388ebf754a4
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2023-11-26 00:49:23 +0530

    initial commit and setup

---
+++ my library/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    Nothing to render
+    </>
+  )
+}
+
+export default App
---
+++ my library/src/components/card/card.jsx
@@ -0,0 +1,11 @@
+import react from 'react'; 
+import './card.module.scss'; 
+
+function Card () {
+    return (
+        <div className='card-wrapper'></div>
+    )
+} 
+
+
+export default Card;
