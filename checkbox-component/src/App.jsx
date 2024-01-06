2cdbe1f00850961c3f19b28071b139b1a6f577d0
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-01-06 16:10:47 +0530

    done

--- checkbox-component/src/App.jsx
+++ checkbox-component/src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import CheckBox from './components/Checkbox'
 
 function App() {
+   
+ let handleChange = (newState) => {
+    console.log('state of the checkbox is', newState)
+  }
 
   return (
-    <> 
-    Nothing for now
-</>
+    <div className='wrapper'> 
+    <h1>CheckBox Component Example</h1>
+    <hr/> 
+    <CheckBox 
+    label = 'Mode' 
+    checked = {true} 
+    onChange = {handleChange}
+    />
+</div>
   )
 }
 
