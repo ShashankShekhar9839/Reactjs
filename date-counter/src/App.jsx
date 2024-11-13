0a34ffe7c5d577f2725f234d4e290e4d0e7f88be
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-11-13 22:48:29 +0530

    Update App.jsx

--- date-counter/src/App.jsx
+++ date-counter/src/App.jsx
@@ -7,30 +7,56 @@ function App() {
   let [count, setCount] = useState(0);
 
   const date = new Date();
-  const currentDate = date.toLocaleDateString();
-  const dayName = date.toLocaleDateString("en-US", { weekday: "long" });
-  console.log(dayName); // e.g., "Wednesday"
+  const adjustedDate = new Date(date);
+  adjustedDate.setDate(date.getDate() + count);
+  const dayName = adjustedDate.toLocaleDateString("en-US", { weekday: "long" });
+
+  const days = count > 0 ? count : -1 * count;
+
+  const handleStepIncreament = () => {
+    setStep((prevStep) => prevStep + 1);
+  };
+
+  const handleStepDecreament = () => {
+    if (step > 0) {
+      setStep((prevStep) => prevStep - 1);
+    }
+  };
+
+  const handleCountIncreament = () => {
+    if (step === 0) {
+      alert("steps are 0, you can not move zero step, increase your steps");
+    }
+    setCount((prevCount) => prevCount + step);
+  };
+
+  const handleCountDecrement = () => {
+    if (step === 0) {
+      alert("steps are 0, you can not move zero step, increase your steps");
+    }
+    setCount((prevCount) => prevCount - step);
+  };
 
   return (
     <>
       <div>
-        <Button label="decrease" />
+        <Button label="decrease" onClick={handleStepDecreament} />
         <span style={{ marginLeft: "20px", marginRight: "20px" }}>
           Step : {step}
         </span>
-        <Button label="Increase" />
+        <Button label="Increase" onClick={handleStepIncreament} />
       </div>
 
       <div style={{ marginTop: "30px", marginBottom: "30px" }}>
-        <Button label="decrease" />
+        <Button label="decrease" onClick={handleCountDecrement} />
         <span style={{ marginLeft: "20px", marginRight: "20px" }}>
           Count : {count}
         </span>
-        <Button label="Increase" />
+        <Button label="Increase" onClick={handleCountIncreament} />
       </div>
       <div>
         <h3>
-          Today is {dayName} on {currentDate}
+          {days} {days > 0 ? "days from" : "days ago"} from Today is {dayName}{" "}
         </h3>
       </div>
     </>
