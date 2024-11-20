04273caa659297e60fc4ec826663ba9b825aea09
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-11-20 12:59:52 +0530

    code cleaning

--- packing-list/src/App.jsx
+++ packing-list/src/App.jsx
@@ -1,8 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
 import PackingList from "./components/PackingList";
-import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 
 function App() {
