74799d5e6091817267d1db1407259f2debc2268b
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-12-09 00:58:23 +0530

    basic states handles

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -1,7 +1,56 @@
+import { useEffect, useReducer } from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Error from "./components/Error";
+import Loader from "./components/Loader";
+
+const initialState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived": {
+      return { ...state, questions: action.payload, status: "ready" };
+    }
+
+    case "dataFailed": {
+      return { ...state, status: "dataFailed" };
+    }
+    default:
+      throw new Error("Unknow Action");
+  }
+}
 
 function App() {
-  return <>Hii there</>;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({
+          type: "dataReceived",
+          payload: data,
+        })
+      )
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Main>
+        {status === "dataFailed" && (
+          <Error errorMessage="Error In Laoding quiz..." />
+        )}
+        {status === "loading" && <Loader />}
+      </Main>
+    </>
+  );
 }
 
 export default App;
---
+++ quiz-app/src/components/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = ({ errorMessage = "Error" }) => {
+  return <h3>{errorMessage}</h3>;
+};
+
+export default Error;
