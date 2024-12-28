cb28679d79634479b16f77f88d0a2113f8f83b94
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-12-28 15:20:41 +0530

    pizzaa-app

--- fastPizza/src/App.jsx
+++ fastPizza/src/App.jsx
@@ -1,16 +1,15 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import Order from "./features/order/Order";
-import CreateOrder from "./features/order/CreateOrder";
-import Cart from "./features/cart/Cart";
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
+import Order, { loader as orderLoader } from './features/order/Order';
+import AppLayout from './ui/AppLayout';
 
-
-// this is the new way of implement routing
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -18,28 +17,33 @@ const router = createBrowserRouter([
 
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
 ]);
 
-const App = () => {
+function App() {
   return <RouterProvider router={router} />;
-};
+}
 
 export default App;
--- fastPizza/src/features/menu/Menu.jsx
+++ fastPizza/src/features/menu/Menu.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
-const Menu = () => {
-  return <div>Menu</div>;
-};
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul className="divide-y divide-stone-200 px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 
 export default Menu;
