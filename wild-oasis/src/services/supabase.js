c57f0dd16909b1b28aaff5cee5b770cd887bed10
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2024-12-29 15:07:12 +0530

    basic setup for wild-oasis

--- wild-oasis/src/services/supabase.js
+++ wild-oasis/src/services/supabase.js
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
-const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabaseUrl = "https://https://xyzcompany.supabase.co";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  "https://xyzcompany.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uaXhpb3J6ZG1hbWx5enZ2bml6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzOTYwNzksImV4cCI6MjA1MDk3MjA3OX0.G9AThegUq3g6AbO42lknssmxJCWO0SNj2lhWeqaj0ok"
+);
 
 export default supabase;
