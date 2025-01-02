0b18d13acefc741d6d34ada8aee640b084865e5f
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-01-03 00:21:47 +0530

    Update Tabs.jsx

--- tabs/src/components/Tabs.jsx
+++ tabs/src/components/Tabs.jsx
@@ -5,30 +5,70 @@ const Tabs = ({
   defaultActive = 0,
   onTabChange = () => {},
   containerClasses = "",
+  threshold = 3,
+  // showMoreTabs = true,
 }) => {
   const [activeIndex, setActiveIndex] = useState(defaultActive);
+  const [showMoreTabs, setShowMoreTabs] = useState(false);
 
-  const handleChange = (event) => {
-    let clickedIndex = event.target.tabIndex;
-    setActiveIndex(clickedIndex);
+  const handleChange = (index) => {
+    setActiveIndex(index);
     if (onTabChange) {
-      onTabChange(clickedIndex);
+      onTabChange(index);
     }
   };
 
-  const rendernav = () => {
-    return children.map((child, index) => {
-      let label = child.props.label;
+  const handleMoreTabClick = () => {
+    setShowMoreTabs(!showMoreTabs);
+  };
 
+  const rendernav = () => {
+    const childrenArray = React.Children.toArray(children);
+    const visibleTabs = childrenArray.slice(0, threshold); // Tabs to show
+    const remainingTabs = childrenArray.slice(threshold); // Tabs to group under "More"
+
+    return (
+      <>
+        {visibleTabs.map((child, index) => {
+          const label = child.props.label || "Default Label";
+          return (
+            <li
+              onClick={() => handleChange(index)}
+              tabIndex={index}
+              key={child.key || index}
+              className={`${index === activeIndex ? "active" : ""}`}
+            >
+              {label}
+            </li>
+          );
+        })}
+        {remainingTabs.length > 0 && (
+          <li className="more-tabs" onClick={handleMoreTabClick}>
+            {`More`}
+            {showMoreTabs && <ul className="dropdown">{renderMoreTabs()}</ul>}
+          </li>
+        )}
+      </>
+    );
+  };
+
+  const renderMoreTabs = () => {
+    const childrenArray = React.Children.toArray(children);
+    const validThreshold = threshold || childrenArray.length;
+
+    return childrenArray.slice(validThreshold).map((child, index) => {
+      const label = child.props.label || "Default Label";
       return (
-        <li
-          onClick={handleChange}
-          tabIndex={index}
-          key={index}
-          className={`${index === activeIndex ? "active" : ""}`}
-        >
-          {label}
-        </li>
+        <>
+          <li
+            onClick={() => handleChange(index + threshold)}
+            tabIndex={index}
+            key={child.key || index} // Use child.key if available
+            className={`${index === activeIndex ? "active" : ""}`}
+          >
+            {label}
+          </li>
+        </>
       );
     });
   };
