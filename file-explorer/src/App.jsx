6ba1514e5c20b1e16ca0345673aa75385ad8d572
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-02-02 12:00:10 +0530

    done file explorer

--- file-explorer/src/App.jsx
+++ file-explorer/src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import FileExplorer from "./components/FileExplorer";
+import explorer from "./data/files";
 
 const App = () => {
-  return <div>App</div>;
+  const [folders, setFolders] = useState(explorer);
+
+  const addFolder = () => {};
+
+  const addFile = () => {};
+
+  return (
+    <div>
+      <FileExplorer
+        explorer={folders}
+        onAddFolder={addFolder}
+        onAddFile={addFile}
+        setFolders={setFolders}
+      />
+    </div>
+  );
 };
 
 export default App;
