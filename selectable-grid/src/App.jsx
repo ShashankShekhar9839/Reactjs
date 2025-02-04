4db09c546e98ff2a631ddc04e33ac6fce245d6b2
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-02-05 00:07:26 +0530

    selectable grid done

---
+++ selectable-grid/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SelectableGrid from "./components/SelectableGrid";
+
+const App = () => {
+  return <div>
+    
+    <SelectableGrid />
+
+  </div>;
+};
+
+export default App;
