4e306a7b06a240e0b8a4762b09ce9202df0fc364
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-03-01 20:03:19 +0530

    toast done

---
+++ toast-component/src/context/toastContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+
+export const ToastContext = createContext();
+
+export const ToastProvider = ({ children }) => {
+  const [toasts, setToasts] = useState([]);
+
+  const addToast = (message, type = "info", duration = 3000) => {
+    const id = Math.random().toString(36).substr(2, 9);
+
+    setToasts((prev) => [...prev, { id, message, type }]);
+    if (duration) {
+      setTimeout(() => {
+        removeToast(id);
+      }, duration);
+    }
+  };
+
+  const removeToast = (id) => {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  };
+
+  return (
+    <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+};
