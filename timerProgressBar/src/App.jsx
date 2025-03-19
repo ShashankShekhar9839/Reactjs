a5d8d21140684efc677119a3346ca35f71572d6a
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2025-03-20 01:01:05 +0530

    done

---
+++ timerProgressBar/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Prgressbar from "./components/Prgressbar";
+
+const App = () => {
+  return (
+    <div>
+      <Prgressbar />
+    </div>
+  );
+};
+
+export default App;
